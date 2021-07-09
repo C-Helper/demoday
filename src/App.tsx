f23fa4620cc3b78b7b2880ce5d98c8532c733b81
cesarfavero <72269544+cesarfavero@users.noreply.github.com>
2021-07-09 15:59:53 -0300

    Re-Upload Project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route } from "react-router-dom"
+
+import { Home } from "./pages/Home";
+import { LoginPage } from "./pages/LoginPage";
+import { Register } from "./pages/Register";
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={Register} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
