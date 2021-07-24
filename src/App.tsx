1b7cfb4a0f81d56860839f174365d695f759ac0f
cesarfavero <cesarfaverodesouza@icloud.com>
2021-07-24 16:41:48 -0300

    default modifications

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route } from "react-router-dom"
 import { Home } from "./pages/Home";
 import { LoginPage } from "./pages/LoginPage";
 import { Register } from "./pages/Register";
-import { Courses } from "./pages/Courses";
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -16,7 +15,6 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/login" exact component={LoginPage} />
         <Route path="/register" exact component={Register} />
-        <Route path="/courses" exact component={Courses} />
       </AuthContextProvider>
     </BrowserRouter>
   );
